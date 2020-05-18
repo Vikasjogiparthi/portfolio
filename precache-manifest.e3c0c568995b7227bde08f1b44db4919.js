8720108562aadee6e68ba255664c8334f43bd09f
vikas jogiparthi <vikasjogiparthi@vikass-MacBook-Air.local>
2020-05-18 09:26:02 -0400

    Updates

---
+++ precache-manifest.e3c0c568995b7227bde08f1b44db4919.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c876d17ff9083824ae7e216bad37a45b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5f4fa6f4b8c22baebdd4",
+    "url": "/portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "b722435debbebd6f8ecf",
+    "url": "/portfolio/static/css/main.b5073daa.chunk.css"
+  },
+  {
+    "revision": "5f4fa6f4b8c22baebdd4",
+    "url": "/portfolio/static/js/2.89549c18.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/portfolio/static/js/2.89549c18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b722435debbebd6f8ecf",
+    "url": "/portfolio/static/js/main.eb910412.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "9c1e2d85a56d00b2e57cf8ee052b420b",
+    "url": "/portfolio/static/media/vikas.9c1e2d85.jpeg"
+  }
+]);
